b9d4a5335000439f857798727990047169df0c6a
arq-gabo <jgabrielguerral@gmail.com>
2020-07-24 10:05:44 -0500

    [FEATURE]Hot Module Replacement

--- webpack-dev-server/src/js/index.js
+++ webpack-dev-server/src/js/index.js
@@ -1,3 +1,12 @@
 import '../css/index.css'
 
-document.body.innerHTML = '<p>Hola Mundo desde Webpack! home</p>';
+import  text from './text'
+
+text()
+
+if(module.hot){
+    module.hot.accept('./text.js', function(){
+        console.log('He recargado en caliente')
+        text()
+    })
+}
