2f1aa2d7292b971e6427a3701e96bfcaa0cc12e3
arq-gabo <jgabrielguerral@gmail.com>
2020-07-27 19:53:19 -0500

    [FEATURE]Final

--- platzi-badges/src/components/App.js
+++ platzi-badges/src/components/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Layout>
+        @El Gabo estuvo por aqui
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
